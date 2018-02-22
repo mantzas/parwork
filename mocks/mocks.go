28fbc1c5c4ccd55f762c72ea48ab524bf09ea8de
Sotirios Mantziaris <smantziaris@gmail.com>
2018-02-22 21:26:18 +0200

    Added mock documentation

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -2,15 +2,23 @@ package mocks
 
 import "github.com/mantzas/parwork"
 
+// MockWork definition
 type MockWork struct{}
 
-func (t MockWork) ID() string      { return "1" }
-func (t MockWork) Do()             {}
+// ID returns a ID
+func (t MockWork) ID() string { return "1" }
+
+// Do executes the work
+func (t MockWork) Do() {}
+
+// GetError returns the work's error
 func (t MockWork) GetError() error { return nil }
 
+// Generator mock for work
 func Generator() parwork.Work {
 	return MockWork{}
 }
 
+// Reporter mock for work
 func Reporter(w parwork.Work) {
 }
