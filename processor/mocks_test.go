e99ec55a6630ffa17178780214e2188256469765
Sotirios Mantziaris <smantziaris@gmail.com>
2018-02-25 14:59:16 +0200

    Moved mocks to file

---
+++ processor/mocks_test.go
@@ -0,0 +1,53 @@
+package processor
+
+import "github.com/mantzas/parwork"
+
+type testWork struct {
+	err      error
+	previous int
+	current  int
+	result   int
+}
+
+func (w *testWork) Do() {
+
+	w.result = w.previous + w.current
+}
+
+func (w *testWork) GetError() error { return w.err }
+
+func (w *testWork) Result() interface{} { return w.current }
+
+type testWorkGenerator struct {
+	current int
+	max     int
+}
+
+func (twg *testWorkGenerator) Generate() parwork.Work {
+
+	if twg.current > twg.max {
+		return nil
+	}
+
+	var w testWork
+	if twg.current == 0 {
+		w.previous = 0
+		w.current = 0
+	} else {
+		w.previous = twg.current - 1
+		w.current = twg.current
+	}
+
+	twg.current++
+
+	return &w
+}
+
+type testCollector struct {
+	results []int
+}
+
+func (fc *testCollector) Collect(w parwork.Work) {
+
+	fc.results = append(fc.results, w.Result().(int))
+}
--- processor/processor_test.go
+++ processor/processor_test.go
@@ -7,56 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type testWork struct {
-	err      error
-	previous int
-	current  int
-	result   int
-}
-
-func (w *testWork) Do() {
-
-	w.result = w.previous + w.current
-}
-
-func (w *testWork) GetError() error { return w.err }
-
-func (w *testWork) Result() interface{} { return w.current }
-
-type testWorkGenerator struct {
-	current int
-	max     int
-}
-
-func (twg *testWorkGenerator) Generate() parwork.Work {
-
-	if twg.current > twg.max {
-		return nil
-	}
-
-	var w testWork
-	if twg.current == 0 {
-		w.previous = 0
-		w.current = 0
-	} else {
-		w.previous = twg.current - 1
-		w.current = twg.current
-	}
-
-	twg.current++
-
-	return &w
-}
-
-type testCollector struct {
-	results []int
-}
-
-func (fc *testCollector) Collect(w parwork.Work) {
-
-	fc.results = append(fc.results, w.Result().(int))
-}
-
 func TestNew(t *testing.T) {
 	assert := assert.New(t)
 	type args struct {
