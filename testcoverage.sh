438935cf39c35ff2f3eade0af5fb91e6bc6c2a08
Sotirios Mantziaris <smantziaris@gmail.com>
2018-03-12 22:54:58 +0200

    Removed coveralls

--- testcoverage.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo 'mode: atomic' > coverage.txt && go list ./... | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp {} && tail -n +2 coverage.tmp >> coverage.txt' && rm coverage.tmp
